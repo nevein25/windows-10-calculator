5bfb656ede4f6b117afc416b73dee00b993d2f0d
nevein <neveinabdelfattah@gmail.com>
2022-06-27 16:14:34 +0200

    added pic

--- script.js
+++ script.js
@@ -37,3 +37,38 @@ let power = () =>{
     screen.innerText=Math.pow(screen.innerText,2);
  
 }
+// buttons.forEach(button => {
+//     button.addEventListener('click', (e) => {
+
+//         switch (e.target.innerText) {
+//             case 'C':
+//             case 'CE':
+//                 screen.innerText = 0;
+//                 break;
+//             case 'del':
+//                screen.innerText = screen.innerText.slice(0,-1);
+//                 break;
+//             case '=':
+//                 try {
+//                     screen.innerText=eval(screen.innerText);
+//                 } catch (error) {
+//                     screen.innerText="ERROR";
+//                 }
+
+//                 break;
+//             default:
+//                 if (screen.innerText == 0) {
+//                     screen.innerText = '';
+//                     screen.innerText += e.target.innerText;
+//                 }
+//                 else {
+//                     screen.innerText += e.target.innerText;
+//                 }
+
+
+//         }
+
+//     })
+// }
+
+// );
