ada3dc5d8be418e230a67f9dfe46ad0dc5276fa3
nevein <neveinabdelfattah@gmail.com>
2022-06-27 16:02:06 +0200

    windows 10 calculator

---
+++ script.js
@@ -0,0 +1,39 @@
+let screen = document.getElementById("screen");
+
+let buttons = Array.from(document.getElementsByClassName("button"));//convert from htmlCollection to array
+
+let calculate = (num) => {
+    if (screen.innerText == 0) {
+        screen.innerText = '';
+        screen.innerText += num;
+    }
+    else {
+        screen.innerText += num;
+    }
+
+}
+
+let equal = () => {
+
+    try {
+        screen.innerText = eval(screen.innerText);
+    } catch (error) {
+        screen.innerText = "ERROR";
+    }
+
+}
+
+let del = () => {
+    screen.innerText = screen.innerText.slice(0,-1);
+}
+
+let clr = () => {
+    screen.innerText = 0;
+}
+let sqrt = () =>{
+    screen.innerText=Math.sqrt(screen.innerText);
+}
+let power = () =>{
+    screen.innerText=Math.pow(screen.innerText,2);
+ 
+}
